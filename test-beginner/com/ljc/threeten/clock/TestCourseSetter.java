abaeb06b5b13dafd8d5dde5d499bd7b2eb62d669
James Gough <jpgough@gmail.com>
2012-07-01 13:24:14 +0100

    add new test cases and mechanisms, one unit test unfinished

--- test-beginner/com/ljc/threeten/clock/TestCourseSetter.java
+++ test-beginner/com/ljc/threeten/clock/TestCourseSetter.java
@@ -1,6 +1,9 @@
 package com.ljc.threeten.clock;
 
+import javax.time.DayOfWeek;
+import javax.time.Duration;
 import javax.time.LocalDate;
+import javax.time.LocalDateTime;
 
 import static org.junit.Assert.*;
 
@@ -27,5 +30,35 @@ public class TestCourseSetter {
 		assertEquals("Month not correct", 11, powderPlotIntervention.getMonth());
 		assertEquals("Day not correct", 2, powderPlotIntervention.getDayOfMonth());
 	}
-
+	
+	@Test
+	public void whatDayWasItOn13September2011() {
+		DayOfWeek dayOfWeek = courseSetter.getDayOfWeek(2011, 9, 13);
+		
+		assertEquals("Day of week is not correct", DayOfWeek.TUESDAY, dayOfWeek);
+	}
+	
+	@Test
+	public void whatIsTheWeekWithinAYearOf13September2011() {
+		long weekOfYear = courseSetter.getWeekOfYear(2011, 9, 13);
+		
+		assertEquals("Week of year is not correct", 37, weekOfYear);
+	}
+	
+	@Test
+	public void durationOfWorldWarII() {
+		LocalDate warBegins = LocalDate.of(1939, 9, 1);
+		LocalDate warEnds = LocalDate.of(1945, 9, 2);
+		
+		Duration duration = courseSetter.differenceBetweenDates(warBegins, warEnds);
+		
+		//FIXME Double check this is the right approach and formulate asserts
+	}
+	
+	@Test
+	public void convertADateTo3pmTime() {
+		LocalDateTime localDateTime = courseSetter.getTimeAtDate(LocalDate.of(2000, 01, 01), 3, 52);
+		
+		assertEquals("Time returned is not correct", LocalDateTime.of(2000, 01, 01, 3, 52), localDateTime);
+	}
 }
