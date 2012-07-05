ffe81cd7b15a498b1e3d4ea9cff8156f40121e4f
Stephen Colebourne <stephen@opengamma.com>
2012-07-05 19:35:27 +0100

    Update ThreeTen dependency

--- src/com/ljc/threeten/date/examples/IDateCourseSetter.java
+++ src/com/ljc/threeten/date/examples/IDateCourseSetter.java
@@ -2,17 +2,17 @@ package com.ljc.threeten.date.examples;
 
 import javax.time.DayOfWeek;
 import javax.time.LocalDate;
-import javax.time.MonthOfYear;
+import javax.time.Month;
 
 public interface IDateCourseSetter {
 
-	public LocalDate getLocalDateFor(int year, MonthOfYear month, int day);
+	public LocalDate getLocalDateFor(int year, Month month, int day);
 
-	public LocalDate getLocalDateBeforeEvent(int year, MonthOfYear month, int day, int offset);
+	public LocalDate getLocalDateBeforeEvent(int year, Month month, int day, int offset);
 	
-	public DayOfWeek getDayOfWeek(int year, MonthOfYear month, int day);
+	public DayOfWeek getDayOfWeek(int year, Month month, int day);
 
-	public long getWeekOfYear(int year, MonthOfYear month, int day);
+	public long getWeekOfYear(int year, Month month, int day);
 
 	public int yearsBetweenDates(LocalDate startDate, LocalDate endDate);
 
--- test-beginner/com/ljc/threeten/date/examples/TestDateCourseSetter.java
+++ test-beginner/com/ljc/threeten/date/examples/TestDateCourseSetter.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import javax.time.DayOfWeek;
 import javax.time.LocalDate;
-import javax.time.MonthOfYear;
+import javax.time.Month;
 
 import org.junit.Test;
 
@@ -14,40 +14,40 @@ public class TestDateCourseSetter {
 	
 	@Test
 	public void testSetCourseForHenryVIIIDeath() {
-		LocalDate henrysDeath = dateCourseSetter.getLocalDateFor(1547, MonthOfYear.JANUARY, 28);
+		LocalDate henrysDeath = dateCourseSetter.getLocalDateFor(1547, Month.JANUARY, 28);
 		
 		assertEquals("Year not correct", 1547, henrysDeath.getYear());
-		assertEquals("Month not correct", MonthOfYear.JANUARY, henrysDeath.getMonthOfYear());
+		assertEquals("Month not correct", Month.JANUARY, henrysDeath.getMonth());
 		assertEquals("Day not correct", 28, henrysDeath.getDayOfMonth());
 	}
 	
 	@Test
 	public void testSetCourseForThreeDaysBeforeGunPowderPlot() {
-		LocalDate powderPlotIntervention = dateCourseSetter.getLocalDateBeforeEvent(1605, MonthOfYear.NOVEMBER, 5, 3);
+		LocalDate powderPlotIntervention = dateCourseSetter.getLocalDateBeforeEvent(1605, Month.NOVEMBER, 5, 3);
 		
 		assertEquals("Year not correct", 1605, powderPlotIntervention.getYear());
-		assertEquals("Month not correct", MonthOfYear.NOVEMBER, powderPlotIntervention.getMonthOfYear());
+		assertEquals("Month not correct", Month.NOVEMBER, powderPlotIntervention.getMonth());
 		assertEquals("Day not correct", 2, powderPlotIntervention.getDayOfMonth());
 	}
 	
 	@Test
 	public void whatDayWasItOn13September2011() {
-		DayOfWeek dayOfWeek = dateCourseSetter.getDayOfWeek(2011, MonthOfYear.SEPTEMBER, 13);
+		DayOfWeek dayOfWeek = dateCourseSetter.getDayOfWeek(2011, Month.SEPTEMBER, 13);
 		
 		assertEquals("Day of week is not correct", DayOfWeek.TUESDAY, dayOfWeek);
 	}
 	
 	@Test
 	public void whatIsTheWeekWithinAYearOf13September2011() {
-		long weekOfYear = dateCourseSetter.getWeekOfYear(2011, MonthOfYear.SEPTEMBER, 13);
+		long weekOfYear = dateCourseSetter.getWeekOfYear(2011, Month.SEPTEMBER, 13);
 		
 		assertEquals("Week of year is not correct", 37, weekOfYear);
 	}
 	
 	@Test
 	public void yearsOfWorldWarII() {
-		LocalDate warBegins = LocalDate.of(1939, MonthOfYear.SEPTEMBER, 1);
-		LocalDate warEnds = LocalDate.of(1945, MonthOfYear.SEPTEMBER, 2);		
+		LocalDate warBegins = LocalDate.of(1939, Month.SEPTEMBER, 1);
+		LocalDate warEnds = LocalDate.of(1945, Month.SEPTEMBER, 2);		
 		int numberOfYears = dateCourseSetter.yearsBetweenDates(warBegins, warEnds);
 		
 		assertEquals("Number of years is not 6", 6, numberOfYears);
