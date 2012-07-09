6a63f20853be7e96bbde426710457aeec135abb8
Stephen Colebourne <scolebourne@joda.org>
2012-07-09 21:55:19 +0100

    Add documentation, tidy, align with others

--- section-basic/com/ljc/threeten/DateTimeCourseSetter.java
+++ section-basic/com/ljc/threeten/DateTimeCourseSetter.java
@@ -6,34 +6,38 @@ import javax.time.LocalDateTime;
 import javax.time.Month;
 import javax.time.ZonedDateTime;
 
+/**
+ * Stub class to be implemented.
+ * See {@link DateTimeCourseSetterTests}.
+ * */
 public class DateTimeCourseSetter {
 
-	public LocalDate getLocalDateFor(int year, Month month, int day) {
-		throw new UnsupportedOperationException();
-	}
+    public LocalDate getLocalDateFor(int year, Month month, int day) {
+        throw new UnsupportedOperationException();
+    }
 
-	public LocalDate getLocalDateBeforeEvent(int year, Month month, int day, int daysBefore) {
-		throw new UnsupportedOperationException();
-	}
+    public LocalDate getLocalDateBeforeEvent(int year, Month month, int day, int daysBefore) {
+        throw new UnsupportedOperationException();
+    }
 
-	public DayOfWeek getDayOfWeek(int year, Month month, int day) {
-		throw new UnsupportedOperationException();
-	}
+    public DayOfWeek getDayOfWeek(int year, Month month, int day) {
+        throw new UnsupportedOperationException();
+    }
 
-	public long getWeekOfYear(int year, Month month, int day) {
-		throw new UnsupportedOperationException();
-	}
+    public long getWeekOfYear(int year, Month month, int day) {
+        throw new UnsupportedOperationException();
+    }
 
-	public DayOfWeek getLastDayOfNextMonth() {
-		throw new UnsupportedOperationException();
-	}
-	
-	public LocalDateTime getTimeAtDate(LocalDate localDate, int hour, int minute) {
-		throw new UnsupportedOperationException();
-	}
+    public DayOfWeek getLastDayOfNextMonth() {
+        throw new UnsupportedOperationException();
+    }
 
-	public ZonedDateTime whatTimeIsItInParisAt(ZonedDateTime dateTime) {	
-		throw new UnsupportedOperationException();
-	}
+    public LocalDateTime getTimeAtDate(LocalDate localDate, int hour, int minute) {
+        throw new UnsupportedOperationException();
+    }
+
+    public ZonedDateTime whatTimeIsItInParisAt(ZonedDateTime dateTime) {
+        throw new UnsupportedOperationException();
+    }
 
 }
--- section-basic/com/ljc/threeten/DateTimeCourseSetterTests.java
+++ section-basic/com/ljc/threeten/DateTimeCourseSetterTests.java
@@ -11,64 +11,76 @@ import javax.time.ZonedDateTime;
 
 import org.junit.Test;
 
+/**
+ * Test to see how basic date-time operations work in ThreeTen.
+ * <p>
+ * Your goal is to make these tests pass.
+ */
+public class DateTimeCourseSetterTests {
 
-public class TestDateTimeCourseSetter {
-	
-	private DateTimeCourseSetter dateCourseSetter = new DateTimeCourseSetter();
-	
+	private DateTimeCourseSetter tool = new DateTimeCourseSetter();
+
+	//-------------------------------------------------------------------------
 	@Test
 	public void testSetCourseForHenryVIIIDeath() {
-		LocalDate henrysDeath = dateCourseSetter.getLocalDateFor(1547, Month.JANUARY, 28);
+		LocalDate henrysDeath = tool.getLocalDateFor(1547, Month.JANUARY, 28);
 		
 		assertEquals("Year not correct", 1547, henrysDeath.getYear());
 		assertEquals("Month not correct", Month.JANUARY, henrysDeath.getMonth());
 		assertEquals("Day not correct", 28, henrysDeath.getDayOfMonth());
 	}
-	
+
+    //-------------------------------------------------------------------------
 	@Test
 	public void testSetCourseForThreeDaysBeforeGunPowderPlot() {
-		LocalDate powderPlotIntervention = dateCourseSetter.getLocalDateBeforeEvent(1605, Month.NOVEMBER, 5, 3);
+		LocalDate powderPlotIntervention = tool.getLocalDateBeforeEvent(1605, Month.NOVEMBER, 5, 3);
 		
 		assertEquals("Year not correct", 1605, powderPlotIntervention.getYear());
 		assertEquals("Month not correct", Month.NOVEMBER, powderPlotIntervention.getMonth());
 		assertEquals("Day not correct", 2, powderPlotIntervention.getDayOfMonth());
 	}
-	
+
+    //-------------------------------------------------------------------------
 	@Test
 	public void whatDayWasItOn13September2011() {
-		DayOfWeek dayOfWeek = dateCourseSetter.getDayOfWeek(2011, Month.SEPTEMBER, 13);
+		DayOfWeek dayOfWeek = tool.getDayOfWeek(2011, Month.SEPTEMBER, 13);
 		
 		assertEquals("Day of week is not correct", DayOfWeek.TUESDAY, dayOfWeek);
 	}
-	
+
+    //-------------------------------------------------------------------------
 	@Test
 	public void whatIsTheWeekWithinAYearOf13September2011() {
-		long weekOfYear = dateCourseSetter.getWeekOfYear(2011, Month.SEPTEMBER, 13);
+		long weekOfYear = tool.getWeekOfYear(2011, Month.SEPTEMBER, 13);
 		
 		assertEquals("Week of year is not correct", 37, weekOfYear);
 	}
-	
+
+    //-------------------------------------------------------------------------
 	//Hard coded unit test to not reveal answer :-)
 	@Test
 	public void getLastDayOfNextMonth() {
 		assertEquals("Last day of Next month is Friday", DayOfWeek.FRIDAY, 
-				dateCourseSetter.getLastDayOfNextMonth());
+				tool.getLastDayOfNextMonth());
 	}
-	
+
+    //-------------------------------------------------------------------------
 	@Test
 	public void convertADateTo3pmTime() {
-		LocalDateTime localDateTime = dateCourseSetter.getTimeAtDate(LocalDate.of(2000, 01, 01), 3, 52);		
+		LocalDateTime localDateTime = tool.getTimeAtDate(LocalDate.of(2000, 01, 01), 3, 52);		
 		assertEquals("Time returned is not correct", LocalDateTime.of(2000, 01, 01, 3, 52), localDateTime);
 	}
-	
+
+    //-------------------------------------------------------------------------
 	@Test
 	public void testDateTimeInParis() {
 		LocalDateTime now = LocalDateTime.now();
 		ZonedDateTime zonedTime = ZonedDateTime.of(now, ZoneId.of("Europe/London"));
-		ZonedDateTime timeInParis = dateCourseSetter.whatTimeIsItInParisAt(zonedTime);
+		ZonedDateTime timeInParis = tool.whatTimeIsItInParisAt(zonedTime);
 		
 		assertEquals("Time returned is not one hour later", now.getHour() + 1, timeInParis.getHour());
 		assertEquals("Minute returned is not the same", now.getMinute(), timeInParis.getMinute());
 		assertEquals("Seconds returned is not the same", now.getMinute(), timeInParis.getMinute());
 	}
+
 }
